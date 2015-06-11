dad813be66b74e1603023c401fec4c84441c0d96
lsdlab <lsdlab@icloud.com>
2015-06-12 04:03:08 +0800

    update requirements.txt

--- article/admin.py
+++ article/admin.py
@@ -9,6 +9,7 @@ class ArticleAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'date_time')
     search_fields = ('title', 'category', 'tag', 'content')
     list_filter = ("date_time",)
+    raw_id_fields = ('tag', )    
     date_hierarchy = 'date_time'
 
 
